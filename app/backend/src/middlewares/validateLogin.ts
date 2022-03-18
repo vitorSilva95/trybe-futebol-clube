7b64653acbafbea67e32c3e46af88771290157eb
Vitor Silva <vitor.o.silva95@gmail.com>
2022-03-18 20:54:20 -0300

    Implementa middleware de validação do login

---
+++ app/backend/src/middlewares/validateLogin.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+
+const validatePassword = (req:Request, res:Response, next:NextFunction) => {
+  const { password } = req.body as { password:string };
+
+  if (!password) {
+    return res.status(400).json({ message: 'inform the password' });
+  }
+  if (password.length <= 6) {
+    return res.status(400).json({ message: 'e-mail is invalid' });
+  }
+  next();
+};
+
+const validateEmail = (req:Request, res:Response, next:NextFunction) => {
+  const { email } = req.body as { email:string };
+  const isValidEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  if (!email) {
+    return res.status(400).json({ message: 'inform the email' });
+  }
+  if (!isValidEmail.test(email)) {
+    return res.status(400).json({ message: 'email is invalid' });
+  }
+
+  next();
+};
+
+const validateLogin = [validateEmail, validatePassword];
+
+export default validateLogin;
