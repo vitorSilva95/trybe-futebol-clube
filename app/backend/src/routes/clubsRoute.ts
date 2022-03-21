c88165c0f9a4ba68e617a9b02d8c1d1c72e73c98
Vitor Silva <vitor.o.silva95@gmail.com>
2022-03-21 20:22:14 -0300

    Implementa rota clubs

--- app/backend/src/routes/clubsRoute.ts
+++ app/backend/src/routes/clubsRoute.ts
@@ -1,13 +1,15 @@
 import { Router, Request, Response } from 'express';
 import StatusCode from '../enums/StatusCode';
+import ClubController from '../controllers/ClubController';
 
 const { OK } = StatusCode;
 
 const clubsRoute = Router();
+const clubController = new ClubController();
 
-clubsRoute.get('/', (req: Request, res: Response) => {
-  const messageOK = { message: 'ROTA CLUBS OK' };
-  return res.status(OK).json(messageOK);
+clubsRoute.get('/', async (req: Request, res: Response) => {
+  const data = await clubController.getAll();
+  return res.status(OK).json(data);
 });
 
 export default clubsRoute;
