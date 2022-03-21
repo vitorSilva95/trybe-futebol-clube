e77455b98df13b3ffe17022ed46fe06b3ec00fff
Vitor Silva <vitor.o.silva95@gmail.com>
2022-03-21 12:00:45 -0300

    Cria mock de usuario

---
+++ app/backend/src/tests/mocks.ts
@@ -0,0 +1,10 @@
+const userPayload ={
+  id: 1,
+  username: "Admin",
+  role: "admin",
+  password:'$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+  email: "admin@admin.com"
+}
+
+
+export { userPayload }
