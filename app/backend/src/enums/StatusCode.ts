25c018138449cf52e9e260213e5b852e3dfb1deb
Vitor Silva <vitor.o.silva95@gmail.com>
2022-03-21 11:59:22 -0300

    Implementa enum com codigos de erros

---
+++ app/backend/src/enums/StatusCode.ts
@@ -0,0 +1,12 @@
+enum StatusCode {
+  OK = 200,
+  UNPROCESSABLE_ENTITY = 422,
+  CREATED = 201,
+  NO_CONTENT = 204,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+  UNAUTHORIZED = 401,
+  INTERNAL_SERVER_ERROR = 500,
+}
+
+export default StatusCode;
