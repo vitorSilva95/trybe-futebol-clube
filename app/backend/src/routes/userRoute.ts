67365b262f70ecf75b3f35af326b55ba71f216a2
Vitor Silva <vitor.o.silva95@gmail.com>
2022-03-21 17:32:40 -0300

    Implementa rota login/validate

--- app/backend/src/routes/userRoute.ts
+++ app/backend/src/routes/userRoute.ts
@@ -1,6 +1,10 @@
 import { Router, Request, Response } from 'express';
+import authMiddleware from '../middlewares/authMiddleware';
 import UserController from '../controllers/userController';
 import validateLogin from '../middlewares/validateLogin';
+import StatusCode from '../enums/StatusCode';
+
+const { OK } = StatusCode;
 
 const userRouter = Router();
 const userController = new UserController();
@@ -10,6 +14,13 @@ userRouter.post('/', validateLogin, async (req: Request, res: Response) => {
   const data = await userController.login(email, password);
 
   res.status(data.status).json(data.response);
+});
+
+userRouter.get('/validate', authMiddleware, async (req: Request, res: Response) => {
+  const { authorization } = req.headers as { authorization: string };
+  const role = userController.validateToken(authorization);
+
+  res.status(OK).send(role);
 });
 
 export default userRouter;
