4827358f34dda3800461db979487ddb778e74638
Vitor Silva <vitor.o.silva95@gmail.com>
2022-03-21 17:33:34 -0300

    Função generateToken

--- app/backend/src/utils/generateToken.ts
+++ app/backend/src/utils/generateToken.ts
@@ -1,10 +1,9 @@
-import * as fs from 'fs';
 import * as jwt from 'jsonwebtoken';
+import FsSecretKey from './FsSecretKey';
 import User from '../interfaces/User';
 
 const generateToken = (payload:User) : string => {
-  const secretKey = fs.readFileSync('jwt.evaluation.key', 'utf-8');
-
+  const secretKey = FsSecretKey();
   const token = jwt.sign(payload, secretKey, {
     algorithm: 'HS256',
     expiresIn: '60h',
