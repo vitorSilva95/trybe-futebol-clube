e85523feb3392d6ef8e078ffeb2dacdf52694a3a
Vitor Silva <vitor.o.silva95@gmail.com>
2022-03-21 17:33:13 -0300

    Implementa metodo validateToken no services

--- app/backend/src/services/UserServices.ts
+++ app/backend/src/services/UserServices.ts
@@ -1,3 +1,4 @@
+import * as jwt from 'jsonwebtoken';
 import generateToken from '../utils/generateToken';
 import UserModel from '../database/models/User';
 import { Login, Error } from '../interfaces/Login';
@@ -8,6 +9,8 @@ const { OK, UNAUTHORIZED } = StatusCode;
 
 class UserServices {
   userModel = UserModel;
+
+  jwtDecode = jwt;
 
   async login(email:string, bodyPassword:string):Promise<Login | Error> {
     const data = await this.userModel.findOne({ where: { email }, raw: true });
@@ -23,6 +26,11 @@ class UserServices {
     };
 
     return { status: OK, response: payload };
+  }
+
+  validateToken(token:string) : string {
+    const role = this.jwtDecode.decode(token, { complete: true });
+    return role?.payload.role;
   }
 }
 
