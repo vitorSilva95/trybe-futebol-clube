70da6f11384484767953741d17a0510a95e1841e
Vitor Silva <vitor.o.silva95@gmail.com>
2022-03-21 21:04:51 -0300

    Implementa metodo getById no controller

--- app/backend/src/controllers/ClubController.ts
+++ app/backend/src/controllers/ClubController.ts
@@ -8,6 +8,11 @@ class UserController {
     const data = await this.clubServices.getAll();
     return data;
   }
+
+  async getById(id: number):Promise<Club | null> {
+    const club = await this.clubServices.getById(id);
+    return club;
+  }
 }
 
 export default UserController;
