ae1f9b4057757a0204f61a29b476bb08ce4580d6
Vitor Silva <vitor.o.silva95@gmail.com>
2022-03-23 19:51:12 -0300

    Adiciona novos verbos na rota Matchs

--- app/backend/src/routes/matchsRoute.ts
+++ app/backend/src/routes/matchsRoute.ts
@@ -1,15 +1,29 @@
 import { Router, Request, Response } from 'express';
+import authMiddleware from '../middlewares/authMiddleware';
 import MatchController from '../controllers/MatchController';
 import StatusCode from '../enums/StatusCode';
+import validateMatch from '../middlewares/validateMatch';
 
-const { OK } = StatusCode;
+const { OK, CREATED } = StatusCode;
 
 const matchsRoute = Router();
 const matchController = new MatchController();
 
 matchsRoute.get('/', async (req: Request, res: Response) => {
-  const matchs = await matchController.getAll();
+  const { inProgress } = req.query as { inProgress: string | undefined };
+  const matchs = await matchController.getAll(inProgress);
   return res.status(OK).json(matchs);
+});
+
+matchsRoute.post('/', authMiddleware, validateMatch, async (req: Request, res: Response) => {
+  const match = await matchController.create(req.body);
+  return res.status(CREATED).json(match);
+});
+
+matchsRoute.patch('/:id/finish', authMiddleware, async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const matchUpdate = await matchController.update(+id);
+  return res.status(OK).json(matchUpdate);
 });
 
 export default matchsRoute;
