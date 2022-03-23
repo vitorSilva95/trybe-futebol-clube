12df1684e238888f9995bc61e6207fe948e6fb1a
Vitor Silva <vitor.o.silva95@gmail.com>
2022-03-23 19:49:39 -0300

    Implementa teste da rota /matchs

--- app/backend/src/tests/matchs.test.ts
+++ app/backend/src/tests/matchs.test.ts
@@ -3,13 +3,13 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Match from '../database/models/Match'
+import Match from '../database/models/Match';
 import { IMatch } from '../interfaces/Match';
 import { Response } from 'superagent';
-import { matchsPayload } from './mocks'
+import { createMatch, matchsPayload } from './mocks'
 import StatusCode from  '../enums/StatusCode';
 
-const { OK ,UNAUTHORIZED } = StatusCode;
+const { CREATED, OK} = StatusCode;
 
 chai.use(chaiHttp);
 
@@ -17,18 +17,18 @@ const { expect } = chai;
 
 describe('Testa a rota /matchs', () => {
 
-  describe('Testa a função get all', ()=>{
+  describe('Testa se tem os retornos certos', ()=>{
     let chaiHttpResponse: Response;
   
   before(async () => {
-    sinon.stub(Match, "findAll").resolves(matchsPayload as IMatch[]);
+    sinon.stub(Match, "findAll").resolves(matchsPayload as Match[]);
   });
 
     after(()=>{
       (Match.findAll as sinon.SinonStub).restore();
     })
 
-    it('verifica se retorna todos as Partidas com sucesso', async () => {
+    it('verifica se retorna todas as partidas', async () => {
       chaiHttpResponse = await chai
         .request(app)
         .get('/matchs')
@@ -36,8 +36,8 @@ describe('Testa a rota /matchs', () => {
         
 
         expect(chaiHttpResponse).to.be.status(OK);
+        expect(chaiHttpResponse.body).to.be.an('array')
         expect(chaiHttpResponse.body).to.deep.equal(matchsPayload);
-        expect(chaiHttpResponse.body).to.be.an('array');
     })
   });
 });
