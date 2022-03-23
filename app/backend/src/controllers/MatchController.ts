082d7f2f8aa4eeb4149025dbaa6e1fff9e9448c5
Vitor Silva <vitor.o.silva95@gmail.com>
2022-03-23 19:50:08 -0300

    Implementa MarchController

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -1,11 +1,22 @@
 import MatchServices from '../services/MatchServices';
+import { IMatch } from '../interfaces/Match';
 
 class MatchController {
   matchServices = new MatchServices();
 
-  async getAll() {
-    const matchs = await this.matchServices.getAll();
+  async getAll(inProgress:string | undefined) {
+    const matchs = await this.matchServices.getAll(inProgress);
     return matchs;
+  }
+
+  async create(payload:IMatch): Promise<IMatch> {
+    const match = await this.matchServices.create(payload);
+    return match;
+  }
+
+  async update(id:number) {
+    const matchUpdate = await this.matchServices.update(id);
+    return matchUpdate;
   }
 }
 
